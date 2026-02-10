9c186ed498aaf755f29d62f8fc5eb98e92589e3f
thepackour <thepackour5610@gmail.com>
2026-02-10 17:37:44 +0900

    도메인, DTO 완성 이슈: https://github.com/thepackour/dongne-reservation/issues/2#issue-3876792019

---
+++ src/main/java/com/dongne/reservation/domain/Timeslot.java
@@ -0,0 +1,44 @@
+package com.dongne.reservation.domain;
+
+import com.dongne.reservation.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Timeslot extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    @NotNull
+    @Column(name = "start_at")
+    private LocalDateTime startAt;
+
+    @NotNull
+    @Column(name = "end_at")
+    private LocalDateTime endAt;
+
+    @NotNull
+    @Column(name = "slot_capacity")
+    private Integer slotCapacity;
+
+    @Column(name = "is_open")
+    private Boolean isOpen;
+
+    @OneToOne(mappedBy = "timeslot", cascade = CascadeType.ALL)
+    private Reservation reservation;
+
+}
---
+++ src/main/java/com/dongne/reservation/domain/User.java
@@ -0,0 +1,42 @@
+package com.dongne.reservation.domain;
+
+import com.dongne.reservation.domain.common.BaseEntity;
+import com.dongne.reservation.enums.UserStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @OneToMany(mappedBy = "user",  cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Reservation> reservationList;
+
+}
