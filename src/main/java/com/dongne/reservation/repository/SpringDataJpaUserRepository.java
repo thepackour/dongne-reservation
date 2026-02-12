eca830220cffbc9a29cfe0b62369a8e910117f39
thepackour <thepackour5610@gmail.com>
2026-02-12 10:38:00 +0900

    SpringDataJpaUserRepository.java에 findByEmail 추가

--- src/main/java/com/dongne/reservation/repository/SpringDataJpaUserRepository.java
+++ src/main/java/com/dongne/reservation/repository/SpringDataJpaUserRepository.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @Primary
 public interface SpringDataJpaUserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
