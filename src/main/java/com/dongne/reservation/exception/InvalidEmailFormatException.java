beaa2b6283c2ae86d44172c23ff3bcf294947ee1
thepackour <thepackour5610@gmail.com>
2026-02-13 22:17:38 +0900

    InvalidEmailFormatException 삭제 @Email 검증으로 변경

--- src/main/java/com/dongne/reservation/exception/InvalidEmailFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.dongne.reservation.exception;
-
-public class InvalidEmailFormatException extends RuntimeException {
-    public InvalidEmailFormatException(String message) {
-        super(message);
-    }
-}
