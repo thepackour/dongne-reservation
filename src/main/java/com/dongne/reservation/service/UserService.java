65b7da80baf21092743e84c9a0665ea9889b1e95
thepackour <thepackour@cau.ac.kr>
2026-02-14 16:44:02 +0900

    login 로직 구현

--- src/main/java/com/dongne/reservation/service/UserService.java
+++ src/main/java/com/dongne/reservation/service/UserService.java
@@ -1,22 +1,33 @@
 package com.dongne.reservation.service;
 
 import com.dongne.reservation.domain.User;
+import com.dongne.reservation.enums.UserRole;
 import com.dongne.reservation.enums.UserStatus;
 import com.dongne.reservation.exception.DuplicateEmailException;
+import com.dongne.reservation.exception.LoginFailedException;
+import com.dongne.reservation.jwt.JwtTokenProvider;
 import com.dongne.reservation.repository.UserRepository;
+import com.dongne.reservation.web.dto.LoginRequest;
 import com.dongne.reservation.web.dto.SignupRequest;
 import com.dongne.reservation.web.dto.SignupResponse;
+import jakarta.validation.Valid;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder pwEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public UserService(UserRepository userRepository, BCryptPasswordEncoder pwEncoder) {
+    public UserService(UserRepository userRepository,
+                       BCryptPasswordEncoder pwEncoder,
+                       JwtTokenProvider jwtTokenProvider) {
         this.userRepository = userRepository;
         this.pwEncoder = pwEncoder;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public SignupResponse register(SignupRequest signupRequest) {
@@ -31,7 +42,8 @@ public class UserService {
                 .password(pwEncoder.encode(signupRequest.getPassword()))
                 .name(signupRequest.getName())
                 .status(UserStatus.ACTIVE)
-                .reservationList(null).build();
+                .reservationList(null)
+                .role(UserRole.USER).build();
         User res = userRepository.save(user);
 
         // 반환 DTO 생성
@@ -39,5 +51,13 @@ public class UserService {
         signupResponse.setId(res.getId());
         signupResponse.setEmail(res.getEmail());
         return signupResponse;
+    }
+
+    public String login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new LoginFailedException("등록되지 않은 이메일입니다."));
+        if (!pwEncoder.matches(request.getPassword(), user.getPassword())) throw new LoginFailedException("비밀번호가 틀렸습니다.");
+
+        return jwtTokenProvider.createToken(user.getEmail(), List.of(user.getRole().toString()));
     }
 }
