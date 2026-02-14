4c802b7124f04f94dea6cb74ad9c4c0a45885280
thepackour <thepackour@cau.ac.kr>
2026-02-15 00:29:14 +0900

    장소 조회 기능 구현

---
+++ src/main/java/com/dongne/reservation/web/controller/PlaceController.java
@@ -0,0 +1,28 @@
+package com.dongne.reservation.web.controller;
+
+import com.dongne.reservation.common.response.ApiResponse;
+import com.dongne.reservation.service.PlaceService;
+import com.dongne.reservation.web.dto.PlaceResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlaceController {
+
+    private final PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/places")
+    public ResponseEntity<ApiResponse<Page<PlaceResponse>>> getPlaces(Pageable pageable) {
+        // pageable 쓰면 @RequestParam 없이 query parameter 받을 수 있다고 함
+        Page<PlaceResponse> result = placeService.getPlaces(pageable);
+        return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), "조회에 성공했습니다.", result));
+    }
+}
