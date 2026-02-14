a36a3c0a04d9705ef4780f4c65e36544dd292fbc
thepackour <thepackour@cau.ac.kr>
2026-02-15 00:03:46 +0900

    SpringDataJpaReservationRepository 추가

---
+++ src/main/java/com/dongne/reservation/repository/SpringDataJpaReservationRepository.java
@@ -0,0 +1,14 @@
+package com.dongne.reservation.repository;
+
+import com.dongne.reservation.domain.Reservation;
+import jakarta.transaction.Transactional;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Primary
+@Transactional
+public interface SpringDataJpaReservationRepository extends JpaRepository<Reservation, Integer> {
+
+}
