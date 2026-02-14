45022c42d1465fbd367fd09880e0f628c1a32fbd
thepackour <thepackour@cau.ac.kr>
2026-02-15 01:04:20 +0900

    특정 장소 조회 로직 구현

--- src/main/java/com/dongne/reservation/web/controller/PlaceController.java
+++ src/main/java/com/dongne/reservation/web/controller/PlaceController.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/places")
 public class PlaceController {
 
     private final PlaceService placeService;
@@ -19,10 +22,16 @@ public class PlaceController {
         this.placeService = placeService;
     }
 
-    @GetMapping("/places")
+    @GetMapping
     public ResponseEntity<ApiResponse<Page<PlaceResponse>>> getPlaces(Pageable pageable) {
         // pageable 쓰면 @RequestParam 없이 query parameter 받을 수 있다고 함
         Page<PlaceResponse> result = placeService.getPlaces(pageable);
         return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), "조회에 성공했습니다.", result));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<PlaceResponse>> getPlace(@PathVariable long id) {
+        PlaceResponse res = placeService.getPlaceById(id);
+        return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), "조회에 성공했습니다.", res));
     }
 }
