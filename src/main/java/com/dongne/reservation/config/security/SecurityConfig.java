7e440d3033a6e9db234503b51181c5e90b752537
thepackour <thepackour@cau.ac.kr>
2026-02-14 12:34:02 +0900

    SecurityConfig CSRF 문제 해결 @Configuration 추가로 해결

--- src/main/java/com/dongne/reservation/config/security/SecurityConfig.java
+++ src/main/java/com/dongne/reservation/config/security/SecurityConfig.java
@@ -3,12 +3,20 @@ package com.dongne.reservation.config.security;
 import com.dongne.reservation.jwt.JwtAuthenticationFilter;
 import com.dongne.reservation.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
@@ -21,15 +29,34 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(httpBasic -> httpBasic.disable())    // 기본 인증 비활성화
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(formLogin -> formLogin.disable())
+                //.formLogin(form -> form.loginProcessingUrl("/users/login"))
+                //.httpBasic(httpBasic -> httpBasic.disable())    // 기본 인증 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/users/signup").permitAll()  // 회원가입 허용
-                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll()   // 로그인 허용
-                        .anyRequest().authenticated()   // 나머지는 인증 필요
+                        .requestMatchers("/users/signup", "/users/login").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 인증정보 주고받도록 허용
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "PUT"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
