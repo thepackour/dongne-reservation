3976604988b410e6a475a5414838bc7689ca7f47
thepackour <thepackour5610@gmail.com>
2026-02-15 20:19:22 +0900

    예약 생성 로직 구현

---
+++ src/main/java/com/dongne/reservation/web/controller/ReservationController.java
@@ -0,0 +1,37 @@
+package com.dongne.reservation.web.controller;
+
+import com.dongne.reservation.common.response.ApiResponse;
+import com.dongne.reservation.service.ReservationService;
+import com.dongne.reservation.web.dto.ReservationRequest;
+import com.dongne.reservation.web.dto.ReservationResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/places/{place_id}/timeslots/{timeslot_id}/reservations")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ReservationResponse>> createReservation(
+        @PathVariable Long place_id,
+        @PathVariable Long timeslot_id,
+        @RequestBody ReservationRequest reservationRequest,
+        HttpServletRequest request
+    ) {
+        String token = request.getHeader("Authorization").substring(7);
+        ReservationResponse res = reservationService.createReservation(place_id, timeslot_id, reservationRequest, token);
+        URI location = URI.create("/places/" + res.getPlaceId() + "/timeslots/" + res.getTimeslotId() + "/reservations/" + res.getId());
+        return ResponseEntity.created(location)
+                    .body(new ApiResponse<>(HttpStatus.CREATED.value(), "예약이 성공적으로 생성되었습니다.", res));
+    }
+}
