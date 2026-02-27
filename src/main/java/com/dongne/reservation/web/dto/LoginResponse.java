1e58f5c81d5a9d30a42e0ab83c6b68dde6681e27
thepackour <thepackour5610@gmail.com>
2026-02-27 14:34:04 +0900

    DTO에 @Data 추가 객체 간 필드 비교 목적

--- src/main/java/com/dongne/reservation/web/dto/LoginResponse.java
+++ src/main/java/com/dongne/reservation/web/dto/LoginResponse.java
@@ -3,15 +3,13 @@ package com.dongne.reservation.web.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Data // 객체 간 필드 비교 때문에 넣음
 public class LoginResponse {
     @NotEmpty
     private String token;
--- src/main/java/com/dongne/reservation/web/dto/ReservationResponse.java
+++ src/main/java/com/dongne/reservation/web/dto/ReservationResponse.java
@@ -6,10 +6,7 @@ import com.dongne.reservation.domain.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -17,6 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class ReservationResponse {
 
     // Reservation 정보
